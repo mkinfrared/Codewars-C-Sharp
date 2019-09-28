2c45b71d99503848de1609fef25bb666654580e1
Mikhail Korotkov <mkinfrared@gmail.com>
2019-09-28 12:30:23 +0300

    refactor: Added grading directories

---
+++ Codewars/8kyu/GrasshopperSummation/Kata.cs
@@ -0,0 +1,23 @@
+﻿namespace Codewars.GrasshopperSummation
+{
+    public class Kata
+    {
+        public static int summation(int num)
+        {
+            int result = Factorial(num);
+
+            return result;
+//            return Enumerable.Range(1, num).Sum();
+//            return Enumerable.Range(1, num).ToArray().Sum();
+        }
+
+        public static int Factorial(int num)
+        {
+            if (num == 1) return 1;
+
+            int result = num + Factorial(num - 1);
+
+            return result;
+        }
+    }
+}
